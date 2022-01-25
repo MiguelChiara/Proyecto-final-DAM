24924e6adf473e1f6dfa2e918f7814e707f00c21
MiguelChiara <74246112+MiguelChiara@users.noreply.github.com>
2022-01-25 14:39:08 +0100

    Add files via upload

---
+++ Android/Spinner/CustomItem.java
@@ -0,0 +1,15 @@
+package com.miguelchiara.pimiguel.Spinner;
+
+// Autor: Miguel Chiara
+
+public class CustomItem {
+    private String spinnerItemName;
+
+    public CustomItem(String nombre){
+        this.spinnerItemName=nombre;
+    }
+
+    public String getSpinnerItemName() {
+        return spinnerItemName;
+    }
+}
---
+++ Android/Spinner/CustomSpinnerAdapter.java
@@ -0,0 +1,52 @@
+package com.miguelchiara.pimiguel.Spinner;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.miguelchiara.pimiguel.R;
+
+import java.util.List;
+
+// Autor: Miguel Chiara
+
+public class CustomSpinnerAdapter extends ArrayAdapter<CustomItem> {
+
+    public CustomSpinnerAdapter(@NonNull Context context, @NonNull List objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView==null){
+            convertView= LayoutInflater.from(getContext()).inflate(R.layout.spinner_layout,parent,false);
+        }
+        CustomItem item = (CustomItem) getItem(position);
+        TextView texto = convertView.findViewById(R.id.spinner_textView);
+        if(item!=null){
+            texto.setText(item.getSpinnerItemName());
+        }
+        return convertView;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView==null){
+            convertView= LayoutInflater.from(getContext()).inflate(R.layout.spinner_down_layout,parent,false);
+        }
+        CustomItem item = (CustomItem) getItem(position);
+        TextView texto = convertView.findViewById(R.id.spinner_textViewdown);
+        if(item!=null){
+            texto.setText(item.getSpinnerItemName());
+        }
+        return convertView;
+    }
+}
